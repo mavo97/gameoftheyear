14e60dc4f769b0d1d06624651042806db5711a39
mavo97 <mavodeveloper@gmail.com>
2020-07-02 18:53:28 -0500

    Show games in the HTML

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,17 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Game } from '../interfaces/interfaces';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
+  private games: Game[] = [];
+
   constructor( private http: HttpClient ) {  }
 
   // tslint:disable-next-line: typedef
   getNominados() {
-      return this.http.get<Game[]>(`${environment.url}/api/goty`);
+
+    if (this.games.length > 0){
+      console.log('Desde cache');
+      return of(this.games);
+    }else{
+      console.log('Desde internet');
+      return this.http.get<Game[]>(`${environment.url}/api/goty`).pipe(
+        tap( games => this.games = games )
+      );
+    }
   }
 
 }
