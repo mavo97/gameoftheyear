ec532b89e3eb26b29be025c4b0c1fb08b2c96833
mavo97 <mavodeveloper@gmail.com>
2020-07-02 18:32:11 -0500

    Get and show games

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
@@ -17,7 +18,8 @@ import { GotyComponent } from './pages/goty/goty.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule
+    ComponentsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/goty/goty.component.ts
+++ src/app/pages/goty/goty.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
+import { Game } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-goty',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GotyComponent implements OnInit {
 
-  constructor() { }
+  constructor( private gameService: GameService ) { }
+
+  games: Game[] = [];
 
   ngOnInit(): void {
+
+    this.gameService.getNominados().subscribe( games => {
+      console.log(games);
+      this.games = games;
+    });
+
   }
 
 }
